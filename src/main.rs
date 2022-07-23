9b669fc3aa97d3af6db293945a0641dcdb71a70e
Ben Smith <bensmith720@gmail.com>
2022-07-23 11:54:42 -0400

    print generic things

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,18 @@
+use std::fmt::{Debug, Display};
+
 fn main() {
-    print_i32(112);
-    print_str("hello world!");
-    print_thing::<i32>(42);
-    print_thing::<&str>("cheeseburgers");
-    print_thing(21);
-    print_thing("oof");
+    // print_i32(112);
+    // print_str("hello world!");
+    // print_thing::<i32>(42);
+    // print_thing::<&str>("cheeseburgers");
+    // print_thing(21);
+    // print_thing("oof");
+    print_things(42, "the answer");
+}
+
+fn print_things<A, B>(thing_a: A, thing_b: B) where A: Display, B: Display {
+    println!("thing_a: {}", thing_a);
+    println!("thing_b: {}", thing_b);
 }
 
 fn print_thing<T: std::fmt::Display>(thing: T)
