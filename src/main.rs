a6a8dc570b1fab6a0aa6a9f665fcb4ea2bc44921
Ben Smith <bensmith720@gmail.com>
2022-07-23 11:33:09 -0400

    print things

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
 fn main() {
-    println!("Hello world");
+    print_i32(112);
+    print_str("hello world!");
+    print_thing::<i32>(42);
+    print_thing::<&str>("cheeseburgers");
+}
+
+fn print_thing<T: std::fmt::Display>(thing: T)
+{
+    println!("The thing is: {thing}");
+}
+
+fn print_i32(num: i32) {
+    println!("The number is {num}");
+}
+
+fn print_str(the_string: &str) {
+    println!("The string is {the_string}");
 }
